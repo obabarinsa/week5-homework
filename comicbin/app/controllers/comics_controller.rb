42f9334dbae8e52165883095a5b507d566f3b4d9
Omobola Babarinsa <obabarinsa@uchicago.edu>
2014-02-08 22:22:48 -0600

    update 4
    fixed image size
    need to update 3 notes

--- comicbin/app/controllers/comics_controller.rb
+++ comicbin/app/controllers/comics_controller.rb
@@ -1,37 +1,26 @@
 class ComicsController <ApplicationController
 
 	def index
-
 		@comics = Comic.all
-
 	end
 
 
 	def show 
-
 		the_comic_id = params["comic_id"]
-
 		@comics = Comic.find_by :id => the_comic_id
-
 		render 'show'
-
 	end
 
 
 	def toast 
-
-		@the_comic_id = params["comic_id"]
-        
-		m = Comic.find_by(:id => @the_comic_id)
+		the_comic_id = params["comic_id"]
+		m = Comic.find_by(:id => the_comic_id)
 		m.destroy
-
 		redirect_to "/comics"
-
 	end 
 
 
 	def new 
-
 	end 
 
 	def create 
@@ -40,9 +29,7 @@ class ComicsController <ApplicationController
     	m.description = params['description']
    		m.image_url = params['image_url']
    		m.save
-
         redirect_to "/comics"
-
 	end 
 
 end 
